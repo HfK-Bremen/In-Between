843b2904499b955ba9b31259e666a3f99682ecb9
Dennis Paul <dennis.paul@hfk-bremen.de>
2012-01-31 18:56:32 +0100

    added xml

---
+++ EchoStepperSteuerung/src/de/hfkbremen/echo/sketches/MotorInterfaceProperties.java
@@ -0,0 +1,11 @@
+
+
+package de.hfkbremen.echo.sketches;
+
+
+public class MotorInterfaceProperties {
+
+    public static final int PREVIEW_INTERFACE_POSTION = 12;
+
+    public static final int CHAPTER_XENAKIS = 18;
+}
